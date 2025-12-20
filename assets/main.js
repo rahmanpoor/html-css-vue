dbe5ebba256ce99d1e1acf84421d0d614a9798ac
Abdolazim Rahmanpour <a.rahmanpour.dev@gmail.com>
2025-12-20 06:35:13 +0330

    first commit

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            message: 'Hello Vue!',
+            isOpen: false,
+            image: './assets/images/logo/logo.jpg',
+        }
+    }
+})
